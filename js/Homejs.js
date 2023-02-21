17f0f83855d37170d613f86a9078305139037b93
Salma Ahmed <64385957+SalmaAhmed20@users.noreply.github.com>
2023-02-22 00:29:56 +0200

    Home page html

---
+++ js/Homejs.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setInterval (currentTimestamp,1000);
+});
+
+function currentTimestamp() {
+    console.log(new Date());
+    var date = new Date();
+    const daylist = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"];
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    document.getElementById("displayDateTime").innerText = daylist[date.getDay()]
+        + " " + monthNames[date.getMonth()] + " " + date.getDate() + " "
+        + " " + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
+}
