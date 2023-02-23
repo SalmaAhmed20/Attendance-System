d12c605b99fe69b930380bf1b42eca3b924c2c90
Maha-Elkomey <47718954+MahaElomey@users.noreply.github.com>
2023-02-23 19:07:36 +0200

    Admin html css

--- js/Adminjs.js
+++ js/Adminjs.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // currentTimestamp();
-    // setInterval(currentTimestamp, 60000);
+    currentTimestamp();
+    setInterval(currentTimestamp, 60000);
+
 });
-function GetNumberofRequest()
-{
-    // get data in local storage
-    
+function currentTimestamp() {
+    console.log(new Date());
+    var date = new Date();
+    const daylist = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"];
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    document.getElementById("displayDateTime").innerText = daylist[date.getDay()]
+        + " " + monthNames[date.getMonth()] + " " + date.getDate() + " "
+        + " " + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
 }
